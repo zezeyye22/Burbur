d3392c90a4d236cc74226172cb6fb455515b60d2
zezeyye22 <104901937+zezeyye22@users.noreply.github.com>
2022-08-24 05:04:16 +0200

    Update deb2prox.sh

--- deb2prox.sh
+++ deb2prox.sh
@@ -151,7 +151,6 @@ pveum usermod admin@pve -group admin
 
 echo "Fetching postinstall script"
 
-
 wget https://raw.githubusercontent.com/zezeyye22/Burbur/main/prox-post-install.sh -c -O /tmp/prox-post-install.sh && chmod +x /tmp/prox-post-install.sh
 if grep -q '#!/usr/bin/env bash' "prox-post-install.sh"; then
   bash prox-post-install.sh
