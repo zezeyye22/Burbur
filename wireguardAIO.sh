be822975038a13d0cdad3fe93144329037174f39
zezeyye22 <104901937+zezeyye22@users.noreply.github.com>
2023-02-08 22:40:43 +0100

    Update wireguardAIO.sh

--- wireguardAIO.sh
+++ wireguardAIO.sh
@@ -148,8 +148,8 @@ function install() {
 function network_conf() {
   echo ""
   echo "### Enable ipv4 Forwarding"
-  sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
-  sysctl -p
+  sed -i 's/[#| ]*net.ipv4.ip_forward[ ]*=[ |0|1]*/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf
+  /bin/sysctl -p
 }
 
 function firewall_conf() {
@@ -233,13 +233,13 @@ function firewall_conf() {
   fi
 
   # Apply rules only if they are not already present
-  if [ ! -z "$RULES_4" ]; then
+  if [ ! -n "$RULES_4" ]; then
     for e in "${RULES_4[@]}"; do
       iptables -C $e > /dev/null 2>&1 || iptables -A $e
     done
   fi
 
-  if [ ! -z "$RULES_6" ]; then
+  if [ ! -n "$RULES_6" ]; then
     for e in "${RULES_6[@]}"; do
       ip6tables -C $e > /dev/null 2>&1 || ip6tables -A $e
     done
