c693f001f594ad44c25a3d907e7cddd4474599f3
zezeyye22 <104901937+zezeyye22@users.noreply.github.com>
2023-02-08 22:58:42 +0100

    Update wireguardAIO.sh

--- wireguardAIO.sh
+++ wireguardAIO.sh
@@ -280,7 +280,7 @@ function wgui_conf() {
   [Service]
   Type=simple
   WorkingDirectory=$WGUI_PATH
-  ExecStart=$WGUI_BIN_PATH/wireguard-ui -bind-address 127.0.0.1:5000
+  ExecStart=$WGUI_BIN_PATH/wireguard-ui -bind-address 127.0.0.1:5001
 
   [Install]
   WantedBy=multi-user.target" > /etc/systemd/system/wgui_http.service
