a8e149aa99e56a250f16786cc234754c79c738a8
zezeyye22 <104901937+zezeyye22@users.noreply.github.com>
2023-02-13 21:11:54 +0100

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -26,3 +26,4 @@ docker volume create portainer_data
 docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 
 apt-get install -y docker-compose
+
